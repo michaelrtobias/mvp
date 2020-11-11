5635f69b9a977a505bafb7e94eb93d3b9f15de1f
Michael Tobias <michaelremitobias@gmail.com>
2020-11-11 00:50:50 -0700

    Stopped webpack from watching

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ const {Client} = require('pg');
 const {Sequelize} = require('sequelize')
 const dotenv = require('dotenv').config();
 
-const conString = "postgres://postgres:postgres@localhost:5432/scorecard";
+// const conString = "postgres://postgres:postgres@localhost:5432/scorecard";
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {logging: false});
 try {
